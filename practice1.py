54fad35447863be3d80d0cdfecf53e170d87b439
anil1596 <anil.khadwal@gmail.com>
2019-07-03 22:32:49 +0530

    first commit (blockchain add/verify)

---
+++ practice1.py
@@ -0,0 +1,23 @@
+name = input('Enter name: ')
+age = input('Enter age:')
+
+
+def print_data(name, age):
+    print(name+age)
+
+
+print_data(name, age)
+
+
+def print_any_data(data1, data2):
+    print(data1+data2)
+
+
+print_any_data('anil', 'khadwal')
+
+
+def calculate_decade(decade):
+    print(decade//10)
+
+
+calculate_decade(23)
