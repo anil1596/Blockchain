9cc66f049114f8ae7af01d3d029191e86abbeb0e
anil1596 <anil.khadwal@gmail.com>
2019-12-16 00:20:13 +0530

    bug fixed - file r/w

--- blockchain.py
+++ blockchain.py
@@ -45,8 +45,8 @@ def load_data():
         open_transactions = json.loads(fileContent[1])
         updated_transactions = []
         for tx in open_transactions:
-            updated_transaction = [OrderedDict(
-                    [('sender',tx['sender']), ('recipient', tx['recipient']), ('amount',tx['amount'])])]
+            updated_transaction = OrderedDict(
+                    [('sender',tx['sender']), ('recipient', tx['recipient']), ('amount',tx['amount'])])
             updated_transactions.append(updated_transaction)
 
         open_transactions = updated_transactions            
